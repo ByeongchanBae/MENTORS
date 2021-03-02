24cb668f742bcba579a715f1036e8df01fcc96f9
Angus Williamson <angus.williamson16@gmail.com>
2021-03-02 11:50:17 +1030

    finished controller

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,13 +1,13 @@
 class MentorsController < ApplicationController
 before_action :authenticate_user!
+before_action :find_mentor, only: [:show, :edit, :update, :destroy]
 
 def index
   @mentors = Mentor.all
 end
 
-# def show
-#   @mentor = Mentor.find(params[:id])
-# end
+def show
+end
 
 def new
   @mentor = Mentor.new
@@ -22,7 +22,27 @@ def create
   end
 end
 
+def edit
+end
+
+def update
+  @mentor.update(mentor_params)
+  redirect_to mentor_path(@mentor)
+end
+
+def destroy
+  @mentor.destroy
+  redirect_to mentors_path
+end
+
 private
+
+def correct_user
+end
+
+def find_mentor
+  @mentor = Mentor.find(params[:id])
+end
 
 def mentor_params
   params.require(:mentor).permit(:title, :description, :price, :speciality)
