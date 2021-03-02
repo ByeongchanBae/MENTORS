adf33f41aad58db48af1a58b84922797e2ff71a6
byeongchan baee <bbae0001@student.monash.edu>
2021-03-02 17:03:15 +1100

    added pages dashboard

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,35 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @mentor = Mentor.find(params[:mentor_id])
+    @booking.mentor = @mentor
+    @booking.user = current_user
+    if @booking.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  private
+
+  def booking_params
+  params.require(:booking).permit(:start_date, :end_date, :price)
+  end
 end
