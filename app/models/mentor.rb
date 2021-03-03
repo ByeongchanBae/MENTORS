e892c35e4bf5c9347ac27b29368b651a3492b2d7
byeongchan baee <bbae0001@student.monash.edu>
2021-03-03 14:47:38 +1100

    added images to mentor

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,6 +1,7 @@
 class Mentor < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_one_attached :avatar
 
   validates :title, presence: true
   validates :description, presence: true
