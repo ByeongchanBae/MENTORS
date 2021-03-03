a21d1bed3a011041ba7b8e39f08c6927bb5b099e
byeongchan baee <bbae0001@student.monash.edu>
2021-03-03 16:23:43 +1100

    added all up to demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database.."
+
+Booking.destroy_all
+Mentor.destroy_all
+User.destroy_all
+
+puts "Database has been cleaned!"
+
+puts "Creating users.."
+
+user_list = [{
+  first_name: "Ryan",
+  last_name: "Bae",
+  email: "user@gmail.com",
+  password: "password"
+},
+{
+  first_name: "Bryan",
+  last_name: "Bae",
+  email: "user2@gmail.com",
+  password: "password"
+}]
+
+User.create!(user_list)
+
+puts "Created #{User.count} users!"
+
+puts "Creating mentors.."
+
+mentor_list = [{
+    title: Faker::Name.name,
+    speciality: Faker::ProgrammingLanguage.name,
+    description: "10 years of experience, graduated from Monash University, Co-Founder of PayPal",
+    price: Faker::Number.number(digits: 2),
+    user: User.first
+},
+{
+    title: Faker::Name.name,
+    speciality: Faker::ProgrammingLanguage.name,
+    description: "10 years of experience, graduated from Monash University, Co-Founder of PayPal",
+    price: Faker::Number.number(digits: 2),
+    user: User.last
+}]
+
+Mentor.create!(mentor_list)
+file = URI.open('https://images.unsplash.com/photo-1588073882362-b4309c60cea0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80')
+mentor = Mentor.first
+mentor.avatar.attach(io: file, filename: 'mentor.png', content_type: 'image/png')
+
+file = URI.open('https://images.unsplash.com/photo-1543269664-56d93c1b41a6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80')
+mentor = Mentor.last
+mentor.avatar.attach(io: file, filename: 'mentor2.png', content_type: 'image/png')
+
+puts "Created #{Mentor.count} mentors!"
+puts "Created mentors!"
