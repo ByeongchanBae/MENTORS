459b9f10bd575c019e91f84354d5bb97900ff584
Angus Williamson <angus.williamson16@gmail.com>
2021-03-04 16:23:23 +1030

    did show map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -2,12 +2,10 @@ import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
-
-const mapElement = document.getElementById('map');
-        const fitMapToMarkers = (map, markers) => {
-        const bounds = new mapboxgl.LngLatBounds();
-        markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-        map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+    const fitMapToMarkers = (map, markers) => {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
     };
 
 const createCustomMarker = (marker) => {
@@ -30,24 +28,22 @@ const addMarkers = (map, markers) => {
       .addTo(map);
   })
 }
-    const initMapbox = () => {
-
-  if (mapElement) { // only build a map if there's a div#map to inject into
-    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    const map = new mapboxgl.Map({
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+    if (mapElement) { // only build a map if there's a div#map to inject into
+      mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+      const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
-    });
-
-    const markers = JSON.parse(mapElement.dataset.markers);
-    addMarkers(map, markers)
-    fitMapToMarkers(map, markers);
-    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-                                      mapboxgl: mapboxgl }));
+      });
+      const markers = JSON.parse(mapElement.dataset.markers);
+      addMarkers(map, markers)
+      fitMapToMarkers(map, markers);
+      map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                        mapboxgl: mapboxgl }));
   }
 };
 
 
-
-
 export { initMapbox };
+
